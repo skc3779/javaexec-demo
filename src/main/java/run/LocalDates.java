c556a94fc232cc4b168af123b3b03ea94776228e
seo kang chun <skc3779@gmail.com>
2015-04-30 14:35:37 +0900

    TimeZone 추가.

--- src/main/java/run/LocalDates.java
+++ src/main/java/run/LocalDates.java
@@ -3,6 +3,7 @@ package run;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
+import java.util.TimeZone;
 
 /**
  * Created by seokangchun on 15. 4. 30..
@@ -10,8 +11,10 @@ import java.time.LocalDate;
 @Slf4j
 public class LocalDates {
     public static void main(String[] args) {
+
+        TimeZone seoul = TimeZone.getTimeZone("Asia/Seoul");
         // 오늘 날짜
-        LocalDate today = LocalDate.now();
+        LocalDate today = LocalDate.now(seoul.toZoneId());
         log.info("today: " + today);
     }
 }
--- src/main/java/run/LocalTimes.java
+++ src/main/java/run/LocalTimes.java
@@ -3,6 +3,7 @@ package run;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalTime;
+import java.util.TimeZone;
 
 /**
  * 지역시간
@@ -10,7 +11,10 @@ import java.time.LocalTime;
 @Slf4j
 public class LocalTimes {
     public static void main(String[] args) {
-        LocalTime rightNow = LocalTime.now();
+
+        TimeZone seoul = TimeZone.getTimeZone("Asia/Seoul");
+
+        LocalTime rightNow = LocalTime.now(seoul.toZoneId());
         log.info("rightNow: " + rightNow);
     }
 }
